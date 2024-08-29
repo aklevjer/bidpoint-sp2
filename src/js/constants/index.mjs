1b98523ffc7a7547d24a6092738d0ec973f01ba1
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-08-29 18:20:33 +0200

    Updates constants with API and default URLs

--- src/js/constants/index.mjs
+++ src/js/constants/index.mjs
@@ -5,6 +5,18 @@ export const API_BASE = "https://v2.api.noroff.dev";
 export const API_AUTH = "/auth";
 export const API_REGISTER = "/register";
 export const API_LOGIN = "/login";
+export const API_AUCTION = "/auction";
+export const API_LISTINGS = "/listings";
+export const API_PROFILES = "/profiles";
+
+export const API_PARAMS_LISTINGS = "?_seller=true&_bids=true&_active=true";
+export const API_PARAMS_PROFILES = "?_listings=true&_wins=true";
 
 export const API_REGISTER_URL = API_BASE + API_AUTH + API_REGISTER;
 export const API_LOGIN_URL = API_BASE + API_AUTH + API_LOGIN;
+export const API_LISTINGS_URL = API_BASE + API_AUCTION + API_LISTINGS;
+export const API_PROFILES_URL = API_BASE + API_AUCTION + API_PROFILES;
+
+// Default image urls
+export const DEFAULT_AVATAR_URL = "/assets/default-avatar.jpg";
+export const DEFAULT_AUCTION_URL = "/assets/default-auction.jpg";
