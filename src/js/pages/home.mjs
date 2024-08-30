35ffbd7a24cf4161e3a1502eb8f74c14dd05cdbf
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-08-31 00:07:14 +0200

    Implements listings rendering on homepage

--- src/js/pages/home.mjs
+++ src/js/pages/home.mjs
@@ -1,3 +1,8 @@
+import { getListings } from "../api/listings/index.mjs";
+import { ListingsHandler } from "../handlers/listings/index.mjs";
+
 export function homePage() {
-  // Logic for home page
+  const endingSoonContainer = document.querySelector(".ending-soon-container");
+  const endingSoonHandler = new ListingsHandler(endingSoonContainer);
+  endingSoonHandler.setCallback(getListings);
 }
