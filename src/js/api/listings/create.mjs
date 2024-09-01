8d75e7174ddc0bec20da88a5e2fffaa076ec33bd
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-09-01 18:19:11 +0200

    Adds API logic for creating a listing

---
+++ src/js/api/listings/create.mjs
@@ -0,0 +1,20 @@
+import { authFetch } from "../index.mjs";
+import { API_LISTINGS_URL } from "../../constants/index.mjs";
+
+export async function createListing(listingData) {
+  const response = await authFetch(API_LISTINGS_URL, {
+    method: "POST",
+    body: JSON.stringify(listingData),
+  });
+
+  const responseData = await response.json();
+
+  if (response.ok) {
+    return responseData;
+  }
+
+  const errorMessage =
+    responseData?.errors[0]?.message || "Failed to create the listing";
+
+  throw new Error(errorMessage);
+}
--- src/js/api/listings/index.mjs
+++ src/js/api/listings/index.mjs
@@ -1,2 +1,3 @@
+export * from "./create.mjs";
 export * from "./get.mjs";
 export * from "./search.mjs";
