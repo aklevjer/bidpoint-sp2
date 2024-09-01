0d7778ea7916e14a1d469998d16f66345c709b08
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-09-01 18:07:34 +0200

    Updates validation for title and other input fields

--- src/js/utils/validation/validateInput.mjs
+++ src/js/utils/validation/validateInput.mjs
@@ -1,24 +1,50 @@
-import { isUsernameValid, isEmailValid, isLengthValid } from "./index.mjs";
 import { toggleInputError } from "../../handlers/ui/index.mjs";
+import {
+  isUsernameValid,
+  isEmailValid,
+  isLengthValid,
+  isDateTimeValid,
+} from "./index.mjs";
 
 export function validateInput(inputElement) {
+  const { name, value } = inputElement;
+
   let isValid = false;
   let message = "";
 
-  switch (inputElement.name) {
+  switch (name) {
     case "name":
-      isValid = isUsernameValid(inputElement.value);
+      isValid = isUsernameValid(value);
       message = "Use only letters, numbers, and _ (max 20 chars)";
       break;
 
     case "email":
-      isValid = isEmailValid(inputElement.value);
+      isValid = isEmailValid(value);
       message = "Please enter a valid @stud.noroff.no email";
       break;
 
     case "password":
-      isValid = isLengthValid(inputElement.value, 8);
+      isValid = isLengthValid(value, 8);
       message = "Password must be at least 8 characters";
+      break;
+
+    case "title":
+      isValid = isLengthValid(value, 1, 280);
+      message = "Title must be 1 to 280 characters";
+      break;
+
+    case "endsAt":
+      isValid = isDateTimeValid(value);
+      message = "Select a future date and time";
+      break;
+
+    case "description":
+      isValid = isLengthValid(value, 0, 280);
+      message = "Description cannot be more than 280 characters";
+      break;
+
+    default:
+      isValid = true;
       break;
   }
 
