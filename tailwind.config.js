8c2c543e4681e8698b0d112fb038a8421f313758
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-09-08 05:12:05 +0200

    Updates tailwind config with border width

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,6 +56,9 @@ module.exports = {
       boxShadow: {
         custom: "0px 0px 8px 0px rgba(0, 0, 0, 0.3)",
       },
+      borderWidth: {
+        6: "6px",
+      },
     },
   },
   plugins: [],
