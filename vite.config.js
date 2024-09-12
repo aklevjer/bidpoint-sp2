b2900efad850d9a91fac655c3320591d16cee21a
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-09-12 14:42:11 +0200

    Configures vite config for deployment

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import { defineConfig } from "vite";
+
+// Simulate __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  build: {
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        listings: resolve(__dirname, "listings/index.html"),
+        singleListing: resolve(__dirname, "listings/listing/index.html"),
+        profile: resolve(__dirname, "profile/index.html"),
+      },
+    },
+  },
+});
