266ae1af31e3bcf262a6ea51a28bc210f0e683dd
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-09-14 18:03:51 +0200

    Refactors date time validation utilities

--- src/js/utils/validation/dateTimeValid.mjs
+++ src/js/utils/validation/dateTimeValid.mjs
@@ -1,10 +1,13 @@
 /**
- * Checks if the provided datetime string represents a future date and time.
+ * Checks if the provided datetime string represents a future date and time within one year.
  *
- * @param {string} dateTimeString - A string representing the date and time to be checked.
- * @returns {boolean} `true` if the provided datetime is in the future, `false` otherwise.
+ * @param {string} dateTime - A string representing the date and time to be checked.
+ * @returns {boolean} `true` if the provided datetime is within one year in the future, `false` otherwise.
  */
-export function isDateTimeValid(dateTimeString) {
-  const date = new Date(dateTimeString);
-  return date > new Date();
+export function isDateTimeValid(dateTime) {
+  const date = new Date(dateTime);
+  const now = new Date();
+  const oneYearFromNow = new Date(now.getFullYear() + 1, now.getMonth(), now.getDate());
+
+  return date > now && date <= oneYearFromNow;
 }
