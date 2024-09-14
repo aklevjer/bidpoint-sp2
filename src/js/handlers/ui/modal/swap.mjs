c270dbf40cc92e4f97454b8f118eaccf0cbfb5b9
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-09-14 18:32:05 +0200

    Fixes JSDoc typos

--- src/js/handlers/ui/modal/swap.mjs
+++ src/js/handlers/ui/modal/swap.mjs
@@ -3,7 +3,7 @@ import { closeModal, openModal } from "./index.mjs";
 /**
  * Handles changing the current modal to a new type.
  *
- * @param {string} newModalType - The type of modal being to switch to (i.e. "login", "register").
+ * @param {string} newModalType - The type of modal to switch to (i.e. "login", "register").
  */
 export function swapModal(newModalType) {
   closeModal();
--- src/js/utils/html/hideElement.mjs
+++ src/js/utils/html/hideElement.mjs
@@ -2,7 +2,7 @@
  * Toggles the visibility of an element by adding or removing the "hidden" class.
  *
  * @param {HTMLElement} element - The element whose visibility is to be toggled.
- * @param {boolean} [shouldHide=false] - Whether to hide or show the element (defaults to false).
+ * @param {boolean} [shouldHide=true] - Whether to hide or show the element (defaults to true).
  */
 export function hideElement(element, shouldHide = true) {
   if (element) {
