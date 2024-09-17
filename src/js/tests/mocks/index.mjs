b08b1c368b5d12e37fe5219dd4962a5256e650ba
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-09-17 15:24:02 +0200

    Adds unit test mocks

---
+++ src/js/tests/mocks/index.mjs
@@ -0,0 +1,4 @@
+export * from "./auth.mock.mjs";
+export * from "./fetch.mock.mjs";
+export * from "./listing.mock.mjs";
+export * from "./localStorage.mock.mjs";
---
+++ src/js/tests/mocks/listing.mock.mjs
@@ -0,0 +1,18 @@
+export const newListing = {
+  title: "Sample Listing",
+  description: "This is a sample description for the listing.",
+  media: [
+    {
+      url: "https://example.com/image.jpg",
+      alt: "Sample Image",
+    },
+  ],
+  endsAt: "2024-12-31T23:59:59Z",
+};
+
+export const listingResponse = {
+  data: {
+    id: "e1b2b3d4-5f67-8a90-bc12-3456789def01",
+  },
+  meta: {},
+};
