e4077908bf72618d2a09939a97c8517f92fedc91
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-09-18 00:43:26 +0200

    Updates vite config with server port

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   build: {
     emptyOutDir: true,
     rollupOptions: {
